1a5289c77543f3b572f87b2b59ca8ba0eba52249
nabeel_kabeer <nabeel.k035@gmail.com>
2017-11-19 13:00:25 +0530

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -1,5 +1,7 @@
 var td_ref = document.getElementsByTagName("td");
 var new_game = document.getElementById('new_game');
+var win = document.getElementById('winner');
+
 var  PLAYER_ONE = -1;
 var PLAYER_TWO = 1;
 
@@ -91,7 +93,6 @@ function repaint_board(board)
 		return ;
 	//initalize handle
 	handle = PLAYER_ONE;
-	var win = document.getElementById('winner');
 	win.textContent = '';
 	//clear internal board state
 	for(var i =0;i < 3;i++)
@@ -157,27 +158,30 @@ function minimax(temp_board,player)
 {
 	//check if any player won the game
 	if(is_win_state(temp_board,human)) return {score: -10};
-	else if(check_draw(temp_board)) return {score: 0};
 	else if(is_win_state(temp_board,ai)) return {score: 10};
+	else if(check_draw(temp_board)) return {score: 0};
 	
 	var ai_max = -1000;
 	var human_min = 10000;
+	var move = {};
 	//select all empty spots and select the one which maximizes output
 	for(var i=0;i < 3;i++){
 		for(var j=0;j < 3;j++){
-			move = {};
 			if(temp_board[i][j] == 0){
 				temp_board[i][j] = player;
-				move.score = minimax(temp_board,player * -1).score;
-				if(player == ai && move.score > ai_max){
+				var temp_score = minimax(temp_board,player * -1).score;
+				if(player == ai && temp_score > ai_max){
 					move.x = i;
 					move.y = j;
+					move.score = temp_score;
 					ai_max = move.score;
+					console.log(move);
 					//if(move.score == 10) break;
 				}
-				else if(player == human && move.score < human_min){
+				else if(player == human && temp_score < human_min){
 					move.x = i;
 					move.y = j;
+					move.score = temp_score;
 					human_min = move.score;
 					//if(move.score == -10) break;
 				}
@@ -196,16 +200,22 @@ function register_move_one(board,k){
 	return function(){
 		x = Math.floor(k/3);
 		y = k%3;
+		if(!is_valid_pos(board,x,y) || is_win(board))
+			return;
 		board[x][y] = human;
 		this.textContent = handle_letter(human);
-		if(is_win(board)) win.textContent = 'Player one wins...';
-		else if(check_draw(board)) win.textContent = 'Game Draw...';
+		if(is_win(board)) {win.textContent = 'Player one wins...';return;}
+		else if(check_draw(board)) {win.textContent = 'Game Draw...';return;}
 		
 		ai_move = minimax(board,ai);
+		console.log(ai_move);
 		board[ai_move.x][ai_move.y] = ai;
-		this.textContent = handle_letter(human);
-		if(is_win(board)) win.textContent = 'Player two wins...';
-		else if(check_draw(board)) win.textContent = 'Game Draw...';
+		td_ref[ai_move.x * 3 + ai_move.y].textContent = handle_letter(ai);
+		if(is_win(board)) {win.textContent = 'Player two wins...';return;}
+		else if(check_draw(board)) {win.textContent = 'Game Draw...';return;}
+		//print board state
+		for(var i=0;i < 3;i++)
+			console.log(board[i]);
 	}
 }
 		
